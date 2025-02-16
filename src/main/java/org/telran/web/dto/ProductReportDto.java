57f6e566e6dfb9aeacc8ddbce479cb0af3019511
kirill-mrotzek <mrotzekkirill@gmail.com>
2025-02-16 15:39:29 +0300

    add report

--- src/main/java/org/telran/web/dto/ProductReportDto.java
+++ src/main/java/org/telran/web/dto/ProductReportDto.java
@@ -2,28 +2,66 @@ package org.telran.web.dto;
 
 import org.telran.web.entity.Product;
 
+/**
+ * Data Transfer Object (DTO) representing product reports.
+ * This class is used to store and transfer data about a product and its associated count,
+ * such as the number of times it was purchased or canceled.
+ */
 public class ProductReportDto {
 
+    /**
+     * The product associated with the report.
+     */
     private Product product;
+
+    /**
+     * The count of occurrences related to the product (e.g., purchase count or cancellation count).
+     */
     private Long count;
 
+    /**
+     * Constructs a new ProductReportDto with the specified product and count.
+     *
+     * @param product The product for which the report is generated.
+     * @param count   The count of occurrences related to the product.
+     */
     public ProductReportDto(Product product, Long count) {
         this.product = product;
         this.count = count;
     }
 
+    /**
+     * Retrieves the product associated with the report.
+     *
+     * @return The product.
+     */
     public Product getProduct() {
         return product;
     }
 
+    /**
+     * Sets the product associated with the report.
+     *
+     * @param product The product to be set.
+     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
+    /**
+     * Retrieves the count of occurrences related to the product.
+     *
+     * @return The count of occurrences.
+     */
     public Long getCount() {
         return count;
     }
 
+    /**
+     * Sets the count of occurrences related to the product.
+     *
+     * @param count The count to be set.
+     */
     public void setCount(Long count) {
         this.count = count;
     }
--- src/main/java/org/telran/web/dto/RevenueReportDto.java
+++ src/main/java/org/telran/web/dto/RevenueReportDto.java
@@ -2,28 +2,66 @@ package org.telran.web.dto;
 
 import java.math.BigDecimal;
 
+/**
+ * Data Transfer Object (DTO) representing revenue reports.
+ * This class is used to store and transfer revenue data grouped by a specified time period.
+ */
 public class RevenueReportDto {
 
+    /**
+     * Represents the time period for which the revenue is calculated.
+     * The value corresponds to the grouping criteria (e.g., hour, day, week, month).
+     */
     private Integer period;
+
+    /**
+     * Represents the total revenue for the specified time period.
+     */
     private BigDecimal revenue;
 
+    /**
+     * Constructs a new RevenueReportDto with the specified period and revenue amount.
+     *
+     * @param period  The time period for which the revenue is calculated (e.g., hour, day, week, month).
+     * @param revenue The total revenue for the specified period.
+     */
     public RevenueReportDto(Integer period, BigDecimal revenue) {
         this.period = period;
         this.revenue = revenue;
     }
 
+    /**
+     * Retrieves the time period of the revenue report.
+     *
+     * @return The period value (e.g., hour, day, week, month).
+     */
     public Integer getPeriod() {
         return period;
     }
 
+    /**
+     * Sets the time period of the revenue report.
+     *
+     * @param period The period value (e.g., hour, day, week, month).
+     */
     public void setPeriod(Integer period) {
         this.period = period;
     }
 
+    /**
+     * Retrieves the total revenue for the specified period.
+     *
+     * @return The total revenue amount.
+     */
     public BigDecimal getRevenue() {
         return revenue;
     }
 
+    /**
+     * Sets the total revenue for the specified period.
+     *
+     * @param revenue The total revenue amount.
+     */
     public void setRevenue(BigDecimal revenue) {
         this.revenue = revenue;
     }
